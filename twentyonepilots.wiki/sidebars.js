eb24633e032d76a177611a27b70d120687279202
Alex Sweeney <x2110311x@gmail.com>
2021-04-03 22:53:35 -0400

    Initial setup and 3 docs
    Introduction
    Original dmaorg files
    05/09/18 - Second journal added

---
+++ twentyonepilots.wiki/sidebars.js
@@ -0,0 +1,13 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Dmaorg',
+      items: [
+        'introduction',
+        'original-dmaorg-files',
+        '05-09-18-second-journal-added'
+      ],
+    },
+  ],
+};
